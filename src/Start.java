4c38e05dbbcacd9b3f9467464f192f9e20fa1512
marina99999 <mvollmer@allegion.com>
2022-03-01 11:04:23 +0100

    Initial commit

---
+++ src/Start.java
@@ -0,0 +1,7 @@
+public class Start {
+    public static void main(String[] args) {
+        System.out.println("Mit diesem Programm haben Sie die Möglichkeit ein Tierinventar zu erstellen oder " +
+                "Abfragen über die Sternis die ein Tier einbringen kann");
+    }
+
+}
