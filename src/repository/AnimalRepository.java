5c0a034460564c6f6ae97856f9d9b7e34d4fd476
Vanessa Nguyen <vanessa.nguyen2@dm.de>
2022-05-25 13:15:41 +0200

    make timeArrayWholeDay and monthArrayWholeYear final

--- src/repository/AnimalRepository.java
+++ src/repository/AnimalRepository.java
@@ -13,8 +13,8 @@ public final class AnimalRepository {
     private List<Bug> animalBug = new ArrayList<>();
     private List<Sea> animalSea = new ArrayList<>();
 
-    private int[] timeArrayWholeDay = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23};
-    private int[] monthArrayWholeYear = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+    private final int[] timeArrayWholeDay = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23};
+    private final int[] monthArrayWholeYear = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
 
     public static AnimalRepository INSTANCE;
 
